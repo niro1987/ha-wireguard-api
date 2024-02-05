4c7ddec91b650fc6e412304ba99eb0b2676b85e7
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-05 22:04:49 +0000

    cleanup and update test requrements

--- src/python_package/hello_world.py
+++
@@ -1,6 +0,0 @@
-"""This is a Sample Python module."""
-
-
-def hello_world():
-    """This is a Sample Python function."""
-    print("Hello World")
--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Tests for the init."""
-
-from python_package.hello_world import hello_world
-
-
-def test_hello():
-    """Test hello."""
-    hello_world()

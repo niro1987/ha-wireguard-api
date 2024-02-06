6597bbcd3d52f702c9080d4cffaf254a142d7f86
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-06 07:04:33 +0000

    init tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,27 @@
 """
 This is a configuration file for pytest containing customizations and fixtures.
 """
+from collections.abc import AsyncGenerator, Generator
+
+import aiohttp
+from aioresponses import aioresponses
+import pytest
+
+from ha_wireguard_api.api import WireguardApiClient
+
+
+@pytest.fixture(name="wireguard_api_client")
+async def client() -> AsyncGenerator[WireguardApiClient, None]:
+    """Return a Spotify client."""
+    async with aiohttp.ClientSession() as session, WireguardApiClient(
+        host="http://localhost",
+        session=session,
+    ) as wireguard_api_client:
+        yield wireguard_api_client
+
+
+@pytest.fixture(name="responses")
+def aioresponses_fixture() -> Generator[aioresponses, None, None]:
+    """Return aioresponses fixture."""
+    with aioresponses() as mocked_responses:
+        yield mocked_responses
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,23 @@
 """Tests for api.py"""
+import aiohttp
+from aioresponses import aioresponses
+from ha_wireguard_api.api import WireguardApiClient
+
+from tests import load_fixture
 
 
-async def test_client():
-    """Happy flow test."""
-    assert True
+async def test_putting_in_own_session(
+    responses: aioresponses,
+) -> None:
+    """Test putting in own session."""
+    responses.get(
+        "http://localhost/data.json",
+        status=200,
+        body=load_fixture("data.json"),
+    )
+    async with aiohttp.ClientSession() as session:
+        client = WireguardApiClient(host="http://localhost/data.json", session=session)
+        await client.get_status()
+        assert client.session is not None
+        await client.close()
+        assert client.session is None

885cb82aac7db19d790e5ce9d018af969d26eec6
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-07 09:43:44 +0000

    fix pylint errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,10 +15,8 @@ from ha_wireguard_api.model import WireGuardPeer
 from tests import load_fixture
 
 
-async def test_client_init() -> None:
+async def test_client_init_host() -> None:
     """Test the initializer of the WireguardApiClient."""
-    with pytest.raises(TypeError):
-        client: WireguardApiClient = WireguardApiClient()
     client: WireguardApiClient = WireguardApiClient("localhost")
     assert client.host == "localhost"
     assert client.session is None
@@ -48,7 +46,7 @@ async def test_client_contextmanager(responses: aioresponses) -> None:
     responses.get("localhost")
     async with WireguardApiClient("localhost") as client:
         assert client.host == "localhost"
-        await client._request("localhost")
+        await client.get_status("localhost")
         assert client.session is not None
     assert client.session is None
 
@@ -59,7 +57,7 @@ async def test_client_timeout(
     """Test WireguardApiClient timeout."""
     responses.get("localhost", timeout=True)
     with pytest.raises(WireGuardTimeoutError) as exc:
-        await client._request("localhost")
+        await client.get_status("localhost")
         assert "Timeout occurred" in str(exc)
 
 
@@ -69,7 +67,7 @@ async def test_client_unexpected_status(
     """Test WireguardApiClient unexpected status."""
     responses.get("localhost", status=500)
     with pytest.raises(WireGuardResponseError) as exc:
-        await client._request("localhost")
+        await client.get_status("localhost")
         assert "Unexpected status" in str(exc)
 
 
@@ -79,18 +77,8 @@ async def test_client_unexpected_content(
     """Test WireguardApiClient unexpected content."""
     responses.get("localhost", content_type="text/plain")
     with pytest.raises(WireGuardResponseError) as exc:
-        await client._request("localhost")
+        await client.get_status("localhost")
         assert "Unexpected content" in str(exc)
-
-
-async def test_client_get_status(
-    client: WireguardApiClient, responses: aioresponses
-) -> None:
-    """Test WireguardApiClient get_status."""
-    responses.get("localhost", repeat=True)
-    response = await client._request("localhost")
-    status = await client.get_status()
-    assert response == status
 
 
 async def test_client_get_peers(
